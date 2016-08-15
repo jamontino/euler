b9625d791d50a059f4acb0ea4e38a08593b16e65
Jamboy <siopaojam@gmail.com>
2016-08-15 15:42:26 +0800

    Test push

--- src/main/java/com/euler/app/Problem6.java
+++ src/main/java/com/euler/app/Problem6.java
@@ -9,6 +9,7 @@ public class Problem6 {
     public double getSumSquareDifferenceOf(int maxDigit) {
         double squareOfSums = Math.pow(IntStream.rangeClosed(1, maxDigit).sum(),2);
         double sumOfSquares = IntStream.rangeClosed(1, maxDigit).reduce(0, (a2, b2) -> (b2 * b2) + a2);
+
         return squareOfSums - sumOfSquares;
     }
 
